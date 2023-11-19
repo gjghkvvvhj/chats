2ee25f3885f49fa72d8fd0edcb53c5bb521ef9b4
saurav <94630933+gjghkvvvhj@users.noreply.github.com>
2023-11-19 07:59:18 +0000

    Update index.js

--- nordserver/index.js
+++ nordserver/index.js
@@ -11,10 +11,10 @@ const io = require('socket.io')(http);
 const users={};
 io.on('connection',socket=>{
     console.log("gggg");
-   socket.on('new-user-joined',name =>{
-    console.log("new user:",name);
+   socket.on('new-user-joined',name,time =>{
+    console.log("new user:",name,time);
 users[socket.id]=name;
-socket.broadcast.emit('user-joined',name);
+socket.broadcast.emit('user-joined',name,time);
    });
  socket.on('deletsend',ids =>{
        if(users[socket.id]=="admin88"){
