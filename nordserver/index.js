24b9bff4ac879886fd8560557bc805f0972890b0
saurav <94630933+gjghkvvvhj@users.noreply.github.com>
2024-03-25 07:29:40 +0000

    Update index.js

--- nordserver/index.js
+++ nordserver/index.js
@@ -41,6 +41,9 @@ const io = require('socket.io')(http);
 
 const users={};
 io.on('connection',socket=>{
+ io.use((socket,next)=>{
+ console.log(socket);
+ })
 
 
    socket.on('sendvss',message =>{
