54b78e3609dfca92aec720f192d64980a4e31f86
saurav <94630933+gjghkvvvhj@users.noreply.github.com>
2024-03-27 12:49:39 +0000

    Update index.js

--- nordserver/index.js
+++ nordserver/index.js
@@ -167,7 +167,7 @@ console.log("drdf"+senddtaaquery1)
 if(getagentsecdata.length>=1){
 var qugupadehd=`UPDATE humman_msg_any SET agent = '`+getagentsecdata[0].id+`' WHERE chatbot_id="`+acceptedaa.bot_id+`" AND chat_id="`+acceptedaa.chat_id+`" AND status="2"`;
 connection.query(qugupadehd);
-let ggdhgd=acceptedaa.chat_id+" this chat is successfully accepted by you"
+let ggdhgd=acceptedaa.chat_id+" accepted"
     
             socket.broadcast.emit(String(acceptedaa.bot_id) + String(getagentsecdata[0].id),{chat_id:acceptedaa.chat_id,bot_id:acceptedaa.bot_id,msg:ggdhgd,type:"2"})
 let setmsg="hey I am "+getagentsecdata[0].name+". I will assist You";
