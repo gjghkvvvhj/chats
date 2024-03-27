2761231c87dd94b9ef5c3a47fa954e0cf404a035
saurav <94630933+gjghkvvvhj@users.noreply.github.com>
2024-03-27 15:35:13 +0000

    Update index.js

--- nordserver/index.js
+++ nordserver/index.js
@@ -133,8 +133,10 @@ console.log("drdf"+senddtaaquery1)
     }
 
      }else{
+	      const time = new Date()
 var senddtaaquery2 = `SELECT * FROM humman_msg_any where chatbot_id="`+senddtaa.bot_id+`" AND chat_id="`+senddtaa.chat_id+`" status="2"`;
   connection.query(senddtaaquery2, function(error, datas){
+	  console.log(datas)
      if(datas.length>=1){
   if( datas[0].agent==""){
  var inserquarry=`INSERT INTO humman_msg_any (id, chatbot_id, msg, chat_id, agent, time, status, browser_agent) VALUES (NULL, '`+senddtaa.bot_id+`', '`+senddtaa.msg+`', '`+senddtaa.chat_id+`', '', '`+time+`', '3', '0');`
@@ -143,8 +145,8 @@ var senddtaaquery2 = `SELECT * FROM humman_msg_any where chatbot_id="`+senddtaa.
 
 
   }else{
-var inserquarry=`INSERT INTO humman_msg_any (id, chatbot_id, msg, chat_id, agent, time, status, browser_agent) VALUES (NULL, '`+senddtaa.bot_id+`', '`+senddtaa.msg+`', '`+senddtaa.chat_id+`', '', '`+time+`', '3', '0');`
-    connection.query(inserquarry)
+var inserquarry4=`INSERT INTO humman_msg_any (id, chatbot_id, msg, chat_id, agent, time, status, browser_agent) VALUES (NULL, '`+senddtaa.bot_id+`', '`+senddtaa.msg+`', '`+senddtaa.chat_id+`', '', '`+time+`', '3', '0');`
+    connection.query(inserquarry4)
   socket.broadcast.emit(String(senddtaa.bot_id) + String(datas[0].agent),{msg:senddtaa.msg,type:"3",chat_id:senddtaa.chat_id,bot_id:senddtaa.bot_id,agent_id:datas[0].agent});
   	  
   }
